1a6c5bdda7991936704f663218f6bc3ce47045aa
vancens002 <t002@vancens.com>
2020-11-30 00:45:49 +0800

    修改composer.json文件

---
+++ src/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Info:
+ * Created by 盐城网诚信息科技有限公司(vancens.com)
+ * User: 阿强(550096055@qq.com)
+ * Date: 2020/11/30
+ * Time: 0:39
+ */
+
+namespace vancens\demo;
+
+
+class Index
+{
+    public function index(){
+        echo 'vancens demo Index index';
+    }
+}
