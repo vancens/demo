5a9b1bd1fe702f840c6ed7965ff865edaae254c2
vancens002 <t002@vancens.com>
2020-11-29 18:50:08 +0800

    添加index.php文件

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # demo
 demo测试文件，随时删除
+增加Index.php文件
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Info:
+ * Created by 盐城网诚信息科技有限公司(vancens.com)
+ * User: 阿强(550096055@qq.com)
+ * Date: 2020/11/29
+ * Time: 18:46
+ */
